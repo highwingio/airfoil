7eaa990dc8423352ff32c143ce697038e4281e55
Jay Zeschin <jay@zeschin.org>
2023-09-27 14:05:54 -0600

    Split railtie

--- datadog/lib/airfoil/datadog.rb
+++ datadog/lib/airfoil/datadog.rb
@@ -1,1 +1,2 @@
 require_relative "middleware/datadog"
+require_relative "datadog_railtie" if defined?(Rails::Railtie)
---
+++ datadog/lib/airfoil/datadog_railtie.rb
@@ -0,0 +1,30 @@
+require "datadog/lambda"
+
+module Airfoil
+  class DatadogRailtie < Rails::Railtie
+    config.datadog_enabled = ENV.fetch("DATADOG_ENABLED", Rails.env.production?).to_s == "true"
+
+    initializer "airfoil.datadog" do
+      if Rails.configuration.datadog_enabled
+        require "ddtrace/auto_instrument"
+
+        ::Datadog::Lambda.configure_apm do |c|
+          c.env = ENV.fetch("SENTRY_ENVIRONMENT", Rails.env).dasherize
+          # downscasing first ensures we don't attempt to snake case things that don't already have dashes
+          c.service = ENV.fetch("AWS_LAMBDA_FUNCTION_NAME", "brokersuite").downcase.underscore
+
+          # Set trace rate via DD_TRACE_SAMPLE_RATE
+          c.tracing.enabled = true
+          c.tracing.instrument :aws
+          c.tracing.instrument :faraday
+          c.tracing.instrument :rest_client
+          c.tracing.instrument :httpclient
+          c.tracing.instrument :http
+          c.tracing.instrument :rails
+        end
+
+        Rails.logger.debug "=====DATADOG LOADED (RAILTIE)====="
+      end
+    end
+  end
+end
